1ea8129dc39fce052ecec80cf3bce24f1c46b74b
RomainKORDALSKI <romain.kordalski@oclock.school>
2024-09-16 12:52:27 +0200

    fix navbar

--- components/data/projects.ts
+++ components/data/projects.ts
@@ -27,7 +27,7 @@ const projects: Project[] = [
     taches:
       "Développement frontend avec Next.js, géolocalisation, création et gestion d'événements, intégration d'une carte interactive",
     imageUrl: "/images/projets/bradenor.png",
-    siteUrl: "/images/projets/bradenor.mov",
+    siteUrl: "/images/projets/bradenorvideo.mov",
     githubUrl: "https://github.com/RomainKORDALSKI/PokeReact",
   },
   {
